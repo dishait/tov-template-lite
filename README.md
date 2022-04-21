6bc17d3dfdd780cc2f82598884191907328ceec1
markthree <1801982702@qq.com>
2022-04-21 14:56:18 +0800

    docs: init

---
+++ README.md
@@ -0,0 +1,50 @@
+<div align="center">
+    <img width="200px" height="200px" src="./public/favicon.png" />
+    <h1>tov-template-lite</h1>
+    <p>非常轻量 vite + vue3 现代化开箱即用模板</p>
+</div>
+
+<br />
+<br />
+
+## 特点 🐳
+
+1. Vite 的
+2. Vue3 的
+3. 文件路由
+4. Api 自动引入
+5. 组件自动引入
+6. VueUse 支持
+7. Unocss 的
+8. TypeScript 的
+9. 暗黑模式支持
+10. pinia 状态管理
+11. pnpm 包管理器
+12. 跳转进度条支持
+13. 插件自动加载支持
+14. Vitest 单元测试支持
+15. 路径别名 `~` 支持
+16. 打包时资源压缩支持
+
+<br />
+<br />
+
+## 组织 🦔
+
+欢迎关注 **帝莎编程**
+
+- [官网](http://dishaxy.dishait.cn/)
+- [Gitee](https://gitee.com/dishait)
+- [Github](https://github.com/dishait)
+- [网易云课堂](https://study.163.com/provider/480000001892585/index.htm?share=2&shareId=480000001892585)
+
+<br />
+<br />
+
+## License
+
+Made with [markthree](./LICENSE)
+
+Published under [MIT License](./LICENSE).
+
+<br />
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Modules from 'vite-plugin-use-modules'
+import Compression from 'vite-plugin-compression'
+import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
@@ -15,15 +17,28 @@ export default defineConfig({
 	plugins: [
 		Vue(),
 		Unocss(),
+
 		Pages({
 			dirs: 'pages'
 		}),
 		Modules({
 			target: 'modules'
 		}),
+		Compression({
+			algorithm: 'brotliCompress'
+		}),
 		Components({
 			dirs: 'components',
 			dts: './types/components.d.ts'
+		}),
+		AutoImport({
+			dts: './types/auto-imports.d.ts',
+			imports: [
+				'vue',
+				'pinia',
+				'vue-router',
+				'@vueuse/core'
+			]
 		})
 	]
 })
